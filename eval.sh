f42c7b25a34a70e30b6b53a67e7ef3d78a5e5df0
lhkhiem28 <lhkhiem28@gmail.com>
2025-10-24 14:42:29 -0400

    .

--- eval.sh
+++ eval.sh
@@ -1,6 +1,6 @@
-lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks arc_challenge  --num_fewshot 25
-lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks truthfulqa_mc2 --num_fewshot 0
-lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks winogrande     --num_fewshot 5
-lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks gsm8k          --num_fewshot 5
-lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks hellaswag      --num_fewshot 10
-lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks mmlu           --num_fewshot 5
+lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks arc_challenge  --apply_chat_template --fewshot_as_multiturn --num_fewshot 25
+lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks truthfulqa_mc2 --apply_chat_template --fewshot_as_multiturn --num_fewshot 0
+lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks winogrande     --apply_chat_template --fewshot_as_multiturn --num_fewshot 5
+lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks gsm8k          --apply_chat_template --fewshot_as_multiturn --num_fewshot 5
+lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks hellaswag      --apply_chat_template --fewshot_as_multiturn --num_fewshot 10
+lm_eval --model hf --model_args pretrained=$1 --device cuda:0 --batch_size 16 --tasks mmlu           --apply_chat_template --fewshot_as_multiturn --num_fewshot 5
